11789a5ad19c806f569ebc9f820a2e7728b823a4
Abraham Trujillo <a.trujillo6801@student.sbccd.edu>
2026-04-29 18:22:41 -0700

    Creation of Visual Studio Solution with commented template spaces for code.

---
+++ DeadPower/DeadPowerGame/MainWindow.xaml.vb
@@ -0,0 +1,89 @@
+﻿Imports System.IO
+Imports Newtonsoft.Json
+
+
+
+Class MainWindow
+
+
+
+    '--------------------------------------------------------------------------------------------------------------
+    'Declarations for Variables that are used across the entire code 
+    '--------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+    '--------------------------------------------------------------------------------------------------------------
+    'Main Game Logic
+    '--------------------------------------------------------------------------------------------------------------
+
+    Public Sub New()
+        InitializeComponent()
+
+        'All the objects that should be created at the start of the game should be created here. This includes the player, the rooms, and any other objects that are necessary for the game to function.
+
+
+
+        'Any functions that update the UI from the start of the game should be called here as well. This includes functions that update the player's stats, the current room's description, and any other UI elements that need to be updated at the start of the game.
+
+    End Sub
+
+
+
+    Private Sub GameLoop()
+
+    End Sub
+
+    'Room Logic
+
+    'Loading a game
+
+    'Saving a game
+
+    'GameOver
+
+    'updating functions defined here
+
+
+
+
+    '--------------------------------------------------------------------------------------------------------------
+    'Interactive UI Logic
+    '--------------------------------------------------------------------------------------------------------------
+
+    'Buttons
+    'things that appear or disappear according to the rooms
+
+
+    '--------------------------------------------------------------------------------------------------------------
+    'Main Character Logic
+    '--------------------------------------------------------------------------------------------------------------
+
+
+
+    'Main Character MOvement and other actions
+
+    'Main Character 
+
+
+
+
+    '--------------------------------------------------------------------------------------------------------------
+    'Enemy Logic
+    '--------------------------------------------------------------------------------------------------------------
+
+    'Enemy Defeat
+
+    'Enemy Movement
+
+    'Enemy Collision
+
+
+
+End Class
